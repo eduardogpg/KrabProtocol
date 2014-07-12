f65e4f892f1b9476281bd37b66af550a39f3dc75
Eduardo78d <eduardo78d@gmail.com>
2014-07-12 18:08:49 -0500

    Registro Usuarios casi listo

--- src/krabprotocol/FXMLRegisterController.java
+++ src/krabprotocol/FXMLRegisterController.java
@@ -10,7 +10,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
+import help.DataBaseConnection;
+import java.sql.ResultSet;
 /**
  * FXML Controller class
  *
@@ -25,9 +29,54 @@ public class FXMLRegisterController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    @FXML
+    private TextField userName;
+    
+    @FXML
+    private TextField name;
+    
+    @FXML
+    private TextField lastName;
+    
+    @FXML
+    private TextField email;
+    
+    @FXML
+    private TextField cellNumber;
+    
+    @FXML
+    private TextField institution;
+    
+    @FXML
+    private PasswordField password;
+    
+    @FXML
+    private PasswordField repeatPassword;
     
     @FXML
     public void registry(){
-        System.out.println("Lalo");
+        if(isValid(this.userName.getText(),this.name.getText(),this.lastName.getText(),this.password.getText(),this.repeatPassword.getText(), cellNumber.getText() , this.email.getText(), this.institution.getText()))
+            System.out.println("Lalo");
+        else
+            System.out.println("No funciono");
+    }
+    
+    private boolean isValid(String userName, String name,String lastName ,String password, String RepeatPassword, String cellNumber, String email, String institutuon){
+        try{
+            int number = Integer.parseInt(cellNumber);
+            if(password.equals(RepeatPassword)){
+                DataBaseConnection c = new DataBaseConnection();
+                ResultSet myResult = c.searchUser(userName);
+                if(myResult == null){
+                    return true;
+                }else
+                    return false;
+                
+            }else
+                return false;
+            
+        }catch(Exception e){ 
+            return false;
+        }
     }
 }
